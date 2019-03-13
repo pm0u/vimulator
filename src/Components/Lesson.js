f244159bce844a3259e6effb3362f543f9547b60
Paul Mourer <paul.mourer@gmail.com>
2019-03-12 21:36:12 -0600

    forgot to add file

--- src/Components/Lesson.js
+++ src/Components/Lesson.js
@@ -2,8 +2,15 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { changeCurrentLesson } from '../redux/actions/currentLesson'
 import { bindActionCreators } from 'redux'
+import { grandMasterWizardKeyHandler } from '../Vim/grandMasterWizardKeyHandler'
 
 export class Lesson extends Component {
+  startLesson = () => {
+    this.props.changeCurrentLesson(this.props.lesson.id)
+    document.addEventListener('keydown', (e) => {
+      grandMasterWizardKeyHandler(e.key)
+    })
+  }
   render() {
     return (
         <li><button onClick={() => this.props.changeCurrentLesson(this.props.lesson.id)}className='link-button'>{this.props.lesson.name}</button></li>
---
+++ src/Vim/grandMasterWizardKeyHandler.js
@@ -0,0 +1,26 @@
+import store from '../redux/store'
+import * as actions from '../redux/actions/vim'
+
+export const grandMasterWizardKeyHandler = (key, store = store) => {
+    // the grandMasterWizardKeyHandler decreeth:
+    // let the keyboard do things!
+    // huzzah!
+    const storeState = store.getState()
+    const keys = storeState.currentLesson.keys
+    switch (keys[key] && key) {
+        case 'h':
+            const col = storeState.vim.cursorPos.col - 1
+            if (storeState.vim.mode === 'NORMAL') {
+                store.dispatch(actions.changeCursorCol(col, storeState.currentLesson))
+            }
+            break
+        case 'l':
+            break
+        case 'j':
+            break
+        case 'k':
+            break
+        default:
+            break
+    }
+}
