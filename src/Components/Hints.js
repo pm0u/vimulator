b80fc069b3099edf5240c6f8c43c8817d08e2d0e
Paul Mourer <paul.mourer@gmail.com>
2019-03-14 16:10:37 -0600

    additional hints and resources displaying

--- src/Components/Hints.js
+++ src/Components/Hints.js
@@ -7,41 +7,50 @@ export class Hints extends Component {
             const hints = this.props.currentLesson.lesson.hints
             return (
                 <div id='hints-content'>
-                <h4>{hints.title}</h4>
-                {hints.text.map(p => {
-                    return (
-                        <p>{p}</p>
-                    )
-                })}
+                    <h4>{hints.title}</h4>
+                    {hints.text.map(p => {
+                        return (
+                            <p>{p}</p>
+                        )
+                    })}
+                    {hints.additional ? <details>
+                        <summary>Additional Hints</summary>
+                        <p>{hints.additional}</p>
+                    </details> : null}
+                    {hints.resources ? <details><summary>Additional Resources</summary> {hints.resources.map(link => {
+                        return (
+                            <><a href={link.href}>{link.text}</a><br />< />
+                                )
+                })}</details> : null }
                 </div>
             )
-        } else {
+                    } else {
             return (
                 <div id='hints-content'>
-                </div>
-            )
-        }
-    }
+                    </div>
+                    )
+                }
+            }
     render() {
         return (
             <div id='hints'>
-            {this.renderHints()}
-                <div id='save-reset'>
-                    <button id='save' className='link-button'>save</button>
-                    <button id='load' className='link-button'>load</button>
-                    <button id='reset' className='link-button'>reset</button>
-                </div>
-            </div>
-        )
-    }
-}
-
+                        {this.renderHints()}
+                        <div id='save-reset'>
+                            <button id='save' className='link-button'>save</button>
+                            <button id='load' className='link-button'>load</button>
+                            <button id='reset' className='link-button'>reset</button>
+                        </div>
+                    </div>
+                    )
+                }
+            }
+            
 const mapStateToProps = state => ({
-    currentLesson: state.currentLesson
-})
-
-
-export default connect(
-    mapStateToProps,
-    null
-)(Hints)
+                        currentLesson: state.currentLesson
+                })
+                
+                
+                export default connect(
+                    mapStateToProps,
+                    null
+                )(Hints)
