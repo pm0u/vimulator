de5d495a99ca4a7fedf03df78dbb292ffcab8910
Paul Mourer <paul.mourer@gmail.com>
2019-03-14 16:12:13 -0600

    cursor position displays

---
+++ src/Components/CursorPos.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+export class CursorPos extends Component {
+    render() {
+        return (
+            <div id='pos-div'>
+                <p id='cursor-position'>{this.props.cursorPos.col + 1},{this.props.cursorPos.row + 1}</p>
+            </div>
+
+        )
+    }
+}
+
+const mapStateToProps = state => ({
+    cursorPos: state.vim.cursorPos
+})
+
+export default connect(
+    mapStateToProps,
+    null
+)(CursorPos)
