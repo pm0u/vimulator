c9d4904818a17486abc2813c43530809668ed1c8
Paul Mourer <paul.mourer@gmail.com>
2019-03-17 09:32:32 -0600

    Added all server files &* modified package.json
    runs locally, seems to work. proxies requests. page title doesn't seem
    to display properly

---
+++ routes/api.js
@@ -0,0 +1,123 @@
+const express = require('express')
+const router = express.Router()
+const usersController = require('../controllers/usersController')
+const unitsController = require('../controllers/unitsController')
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+
+const privateKey = process.env.JWT_PRIVATE_KEY
+const publicKey = process.env.JWT_PUBLIC_KEY
+
+
+const verify = function (req, res, next) {
+    jwt.verify(req.cookies.token, privateKey,
+        (err, decoded) => {
+            if (err) {
+                next({
+                    status: 401,
+                    error: err,
+                    message: 'Unauthorized'
+                })
+            } else {
+                req.userCredentials = decoded
+                console.log('user verified', decoded)
+                next()
+            }
+        })
+}
+
+router
+    .get('/user', verify, (req, res) => {
+        usersController.getUserById(req.userCredentials.ghID).then((user, err) => {
+            res.status(200).send(`${user}`)
+        })
+    })
+    .get('/users', verify, async (req, res) => {
+        const { ghID } = req.userCredentials
+        if (ghID === process.env.MONGODB_ADMIN_ID) {
+            const users = await usersController.getAllUsers()
+            if (users.length > 0) {
+                res.status(200).send(users)
+            } else {
+                next({
+                    status: 204,
+                    error: users,
+                    message: 'No users exist'
+                })
+            }
+        } else {
+            next({
+                status: 401,
+                message: 'Unauthorized'
+            })
+        }
+    })
+    .delete('/user/:ghID', verify, async (req, res, next) => {
+        const { ghID } = req.userCredentials
+        if (ghID === process.env.MONGODB_ADMIN_ID) {
+            const user = await usersController.deleteUserByGHID(req.params.ghID)
+            if (user.n > 0) {
+                res.status(201).send(`user #${req.params.ghID} successfully deleted`)
+            } else {
+                next({
+                    message: 'User does not exist',
+                    error: user,
+                    status: 404
+                })
+            }
+        } else {
+            next({
+                status: 401,
+                message: 'Unauthorized'
+            })
+        }
+    })
+
+router
+    .get('/units', unitsController.getAllUnits)
+    .get('/units/:unitID', unitsController.getUnitByID)
+    .post('/units', verify, async (req, res, next) => {
+        const { ghID } = req.userCredentials
+        if (ghID === process.env.MONGODB_ADMIN_ID) {
+            try {
+                const newUnit = await unitsController.createNewUnit(req.body)
+                res.status(200).send(newUnit)
+            }
+            catch (error) {
+                next({
+                    message: 'New unit not created',
+                    error,
+                    status: 500
+                })
+            }
+        } else {
+            next({
+                status: 401,
+                message: 'Unauthorized'
+            })
+        }
+    })
+    .delete('/units/:unitID', verify, async (req, res, next) => {
+        const { ghID } = req.userCredentials
+        if (ghID === process.env.MONGODB_ADMIN_ID) {
+            const deletedUnit = await unitsController.deleteUnitByID(req.params.unitID)
+            if (deletedUnit.n > 0) {
+                res.status(200).send(deletedUnit)
+            } else {
+                next({
+                    status: 404,
+                    message: 'Unit does not exist',
+                    error: deletedUnit
+                })
+            }
+        } else {
+            next({
+                status: 401,
+                message: 'Unauthorized'
+            })
+
+        }
+    })
+
+
+module.exports = router
---
+++ routes/login.js
@@ -0,0 +1,36 @@
+'use strict'
+const express = require('express')
+const passport = require('passport')
+const router = express.Router()
+const bcrypt = require('bcryptjs')
+const secretToken = process.env.JWT_KEY
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+
+const privateKey = process.env.JWT_PRIVATE_KEY
+const publicKey = process.env.JWT_PUBLIC_KEY
+
+const usersController = require('../controllers/usersController')
+
+const appUrl = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_URL : 'http://localhost:3000'
+
+const domain = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_URL : 'localhost'
+
+
+router
+    .get('/github', passport.authenticate('github'))
+    .get('/github/callback', passport.authenticate('github'), async (req, res) => {
+        const { id: ghID, displayName } = req.user.profile
+
+        let user = await usersController.getUserById(ghID)
+
+        if (user.length === 0) {
+            user = await usersController.newUserAtLogin({ displayName, ghID })
+        }
+        const token = jwt.sign(JSON.stringify(user[0]), privateKey)
+        res.cookie('token', token, { domain: 'vimulator2.surge.sh', path:'/', httpOnly:true })
+        res.redirect(`https://vimulator2.surge.sh`)
+    })
+
+
+module.exports = router
