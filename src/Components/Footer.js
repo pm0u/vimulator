5778096b0bd1ddd845d701104f4a2afe24fa86f8
Paul Mourer <paul.mourer@gmail.com>
2019-03-18 16:31:22 -0600

    removed some unused things, fixed alignment on hints file

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react'
 
 export class Footer extends Component {
+    //<li><a href="#" target='_self' id='change-colors'>dark mode</a></li>
+    //<a href='#' id='lesson-toggle' target='_self'>toggle lessons</a>
+    //<a href='#' id='hint-toggle' target='_self'>toggle hints</a>
+
     render() {
         return (
             <>
                 <footer>
-                    <a href='#' id='lesson-toggle' target='_self'>toggle lessons</a>
                     <div id='footer-list'>
                         <ul id='footer-content'>
                             <li>created by <a href='mailto:paul.mourer@gmail.com'>Paul Mourer</a></li>
                             <li>contribute on <a href='https://www.github.com/pm0u/vimulator'>Github</a></li>
-                            <li><a href="#" target='_self' id='change-colors'>dark mode</a></li>
                         </ul>
                     </div>
-                    <a href='#' id='hint-toggle' target='_self'>toggle hints</a>
                 </footer>
             </>
 
