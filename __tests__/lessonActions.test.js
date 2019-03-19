affb8114296c7de6d65a4ffd9cd959085fabca82
Paul Mourer <paul.mourer@gmail.com>
2019-03-19 09:09:44 -0600

    fixed tests, all passing

--- __tests__/lessonActions.test.js
+++ __tests__/lessonActions.test.js
@@ -1,9 +1,11 @@
 import * as types from '../src/redux/constants/currentLessonActions'
 import * as actions from '../src/redux/actions/currentLesson'
-import { unit1 } from '../src/Vim/units'
-import initialState from '../src/redux/reducers/vim'
 import thunk from 'redux-thunk'
 import configureStore from 'redux-mock-store'
+import { initialState } from './mockState'
+
+
+console.log(initialState)
 
 const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
@@ -11,11 +13,11 @@ const mockStore = configureStore(middlewares);
 describe('Current lesson actions', () => {
     it('should change current lesson', () => {
         const store = mockStore(initialState)
-        const lesson = unit1.lessons[1]
-        const fakeKeyHandler = () => {}
+        const lesson = store.getState().units.lessons[1]
+        const fakeKeyHandler = () => { }
         const expectedAction = {
             type: types.CHANGE_CURRENT_LESSON,
-            newLesson: unit1.lessons[1],
+            newLesson: lesson,
             keyHandler: fakeKeyHandler
         }
         store.dispatch(actions.changeCurrentLesson(lesson, fakeKeyHandler))
