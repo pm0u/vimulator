6c840e385e59467f51ba94f6aac05e719dc2e92e
Paul Mourer <paul.mourer@gmail.com>
2019-03-24 18:49:25 -0600

    added keyboard shortcuts to finishmodal, fix user post
    user post route still had /user, should be /users.. can now use enter or
    r to go to next lesson or restart.

--- src/Components/FinishModal.js
+++ src/Components/FinishModal.js
@@ -5,17 +5,31 @@ import { bindActionCreators } from 'redux'
 import { nextLesson, restartLesson } from '../redux/actions/currentLesson'
 
 export class FinishModal extends Component {
+
+    menuChoice = (e) => {
+        console.log('you pressed a key')
+        const key = e.key
+        if (key === 'Enter') {
+            document.removeEventListener('keydown',this.menuChoice)
+            this.props.nextLesson()
+        } else if (key === 'r') {
+            document.removeEventListener('keydown',this.menuChoice)
+            this.props.restartLesson()
+        }
+    }
+
     render() {
         return (
             <ReactModal
                 isOpen={this.props.finishWindow}
+                onAfterOpen={() => document.addEventListener('keydown',this.menuChoice)}
                 contentLabel='finish modal'
                 className='pop-up-div'
             >
                 <section className='login-window'>
                     <section>
                         <h3>Lesson Completed!</h3>
-                        <p>you can repeat this lesson, move to the next lesson (if you've already finished it, you'll start from the beginning), or close this window and choose a lesson</p>
+                        <p>you can repeat this lesson, move to the next lesson (if you've already finished it, you'll start from the beginning), or close this window and choose a lesson. Press 'r' to restart or 'Enter' to go to next lesson.</p>
                     </section>
                     <div className='finish-buttons'>
                         <button className='link-button' onClick={this.props.restartLesson}>restart this lesson</button>
